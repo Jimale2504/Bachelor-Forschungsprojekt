bd0a49576d73784916a747d1737b9b05b8bd7d9a
Yussuf <jimaleyussuf03mail.com>
2025-12-04 02:25:30 +0100

    README angepasst

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # Bachelor-Forschungsprojekt
 This project uses code from Virtual-catalog-DLL licensed under GPL-3.0.
 Virtual-catalog-DLL from https://github.com/GPenet/Virtual-catalog-DLL
+
+Virtual Catalog Wrapper für ED Sudokus (run.py)
+Dieses Python-Skript (run.py) dient als Wrapper für das C++ Tool sktvcat.exe, um Essentially Different (ED) Sudokus (minimal lexikale Lösungen) aus dem virtuellen Katalog zu generieren.
+
+Alternativ kann man auch den command sktvcat -c0 -v0-[anfang] -v1-[ende] -v2-1 ausführen (anfang und ende halt mit dem Index der Bänder austauschen)
+e.g sktvcat -c0 -v0-0 -v1-415 um alle ED-Sudokus zu printen
+
+📚 Die benötigten Bibliotheken:
+-skbvcminlex .dll
+-skbminlex.dll
+-skgminlex.dll
+-skbgridmin.dll
+
+Virtual-catalog-DLL/
+├── run.py                # Skript
+├── sktvcat.exe           # C++ Tool
+├── skvcat.dll            # Benötigte DLL
+├── skbminlex.dll         # Benötigte DLL
+├── ... (weitere DLLs)
--- Virtual-catalog-DLL/run.py
+++ Virtual-catalog-DLL/run.py
@@ -1,7 +1,10 @@
 import cmd,os
 
-'''Wichtig! mit Index ist das erste Band gemeint, geht von 0-415 und damit sind 
-die 416 einzigartigen erste Bänder zu gemeint siehe  http://www.afjarvis.org.uk/maths/felgenhauer_jarvis_spec1.pdf Seite 4'''
+'''
+Mit Index ist das erste Band gemeint. 
+Der Bereich geht von 0-415 (die 416 einzigartigen ersten Bänder).
+Siehe: http://www.afjarvis.org.uk/maths/felgenhauer_jarvis_spec1.pdf Seite 4
+'''
 
 def run():
   print("Do you want to print all ED SUdokus (y/n)")
@@ -10,13 +13,12 @@ def run():
   if(answer=="y"):
     os.system("sktvcat -c0 -v0-0 -v1-415 -v2-1 > ED_Sudokus.txt") #command + Speicherort  
   else:
-   print("Konfiguration of the first E.D Sudokus bands you want to print")
+   print("Index of the first E.D Sudokus bands you want to print(should be between 0 and 415)")
    start=input()
-   print("Konfiguartion of the last  E.D Sudokus bands you want to print ")
+   print("Index of the last  E.D Sudokus bands you want to print (should be between 0 and 415)")
    end=input()
-   os.system("sktvcat -c0 -v0-{start} -v1-{end} -v2-1 > ED_Sudokus.txt") #command + Speicherort  
-
-
+   print("Solution will be saved in ED_Sudokus.txt")
+   os.system("sktvcat -c0 -v0-{start} -v1-{end} -v2-1 > ED_Sudokus.txt ") #command + Speicherort  
 
 if __name__=="__main__":
   run()    
